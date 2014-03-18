f623fc0d8165343d5ae7c726bcdbbe21f12af14f
José Bollo <jose.bollo@open.eurogiciel.org>
2014-03-18 11:38:26 +0100

    Removing used files
    Change-Id: I335f7388d74646eb9b29ba36c7afb9ed60c6e9db
    Signed-off-by: José Bollo <jose.bollo@open.eurogiciel.org>

--- src/smack-utils.h
+++
@@ -1,7 +0,0 @@
-/* 2014, Copyright Intel & Jose Bollo <jose.bollo@open.eurogiciel.org>, license MIT */
-
-#include "smack-utils-label.h"
-#include "smack-utils-coda.h"
-#include "smack-utils-access.h"
-#include "smack-utils-fs.h"
-
