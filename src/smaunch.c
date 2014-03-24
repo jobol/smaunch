06b750dc07b8f24edb4143d96d423c031e035500
José Bollo <jose.bollo@open.eurogiciel.org>
2014-03-24 17:28:18 +0100

    API revisited and improved
    Change-Id: If78aecaf33763fac3d569588117d4ba1cfc3c852
    Signed-off-by: José Bollo <jose.bollo@open.eurogiciel.org>

--- src/smaunch.c
+++ src/smaunch.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
 #include <assert.h>
 #include <sys/prctl.h>
 #include <linux/securebits.h>
@@ -13,6 +14,8 @@
 #if !defined(SIMULATION)
 #define SIMULATION 0
 #endif
+
+extern char **environ;
 
 static const char *default_fs_key = 0;
 
@@ -26,8 +29,9 @@ int smaunch_init(const char *smackdb, const char *fsdb, const char *defskey)
 	default_fs_key = 0;
 
 #if !SIMULATION
-	if (!smack_fs_mount_point())
+	if (!smack_fs_mount_point()) {
 		return -EACCES;
+	}
 #endif
 
 	result = smaunch_smack_load_database(smackdb);
@@ -50,7 +54,7 @@ int smaunch_is_ready()
 	return !!default_fs_key;
 }
 
-int smaunch_apply(char **keys)
+int smaunch_prepare(char **keys)
 {
 	int result, setdef, rsm, rfs;
 
@@ -80,6 +84,15 @@ int smaunch_apply(char **keys)
 		assert(!result);
 	}
 
+	return 0;
+}
+
+int smaunch_apply()
+{
+	int result;
+
+	assert(smaunch_is_ready());
+
 	result = smaunch_smack_context_apply();
 	if (result)
 		return result;
@@ -105,12 +118,67 @@ int smaunch_drop_caps()
 	return sts ? -errno : 0;
 }
 
-int smaunch_exec(char **keys, const char *filename, char **argv, char **envp);
-int smaunch_fork_exec(char **keys, const char *filename, char **argv, char **envp);
+int smaunch_exec(char **keys, const char *filename, char **argv, char **envp)
+{
+	int result;
 
+	assert(smaunch_is_ready());
+
+	result = smaunch_prepare(keys);
+	if (result)
+		return result;
+
+	result = smaunch_apply();
+	if (result)
+		return result;
+
+	result = execve(filename, argv, envp ? envp : environ);
+	return -errno;
+}
+
+int smaunch_fork_exec(char **keys, const char *filename, char **argv, char **envp)
+{
+	volatile int result;
+	pid_t cpid;
+
+	assert(smaunch_is_ready());
+
+	/* prepare the contexts */
+	result = smaunch_prepare(keys);
+	if (result)
+		return result;
+
+	/* using vfork is really what is wanted */
+	cpid = vfork();
+	if (cpid < 0) {
+		return -errno;
+	}
+
+	if (!cpid) {
+		/* within newly vforked child */
+		result = smaunch_apply();
+		if (!result) {
+			execve(filename, argv, envp ? envp : environ);
+			result = -errno;
+		}
+		_exit(1);
+	}
+
+	/* within vforking parent, after that child exits or execs */
+	assert(cpid > 0);
+	return result;
+}
 
 #ifdef TEST
 #include <stdio.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
+struct timeval          tops[20];
+
+#define lap(x)          gettimeofday(&tops[x],NULL)
+#define timedbl(tv)     ((double)(tv.tv_sec) * (double)1e6 + (double)(tv.tv_usec))
+#define duration(x,y)   (timedbl(tops[y]) - timedbl(tops[x]))
 
 int main(int argc, char** argv)
 {
@@ -119,19 +187,43 @@ int main(int argc, char** argv)
 		{ "%user", NULL },
 		{ "%uid",  NULL }
 	};
+	char *command[] = {
+		"/bin/sh",
+		NULL
+	};
+
+	lap(0);
 
 	substs[0][1] = getenv("USER");
 	substs[1][1] = getenv("UID");
-
 	smaunch_fs_set_substitutions(substs, 2);
 
+	lap(1);
+
 	sts = smaunch_init("db.smack", "db.fs", "restricted");
+
+	lap(2);
+
 	printf("init %d\n", sts);
+	++argv;
 
-	sts = smaunch_apply(++argv);
-	printf("apply %d\n", sts);
+	lap(3);
 
-	system("/bin/sh");
+	sts = smaunch_fork_exec(argv, command[0], command, NULL);
+
+	lap(4);
+
+	printf("forkexec %d\n", sts);
+
+	if (!sts)
+		wait(&sts);
+
+	printf("TIMES:\n");
+	printf("  substs   %lf\n",duration(0,1));
+	printf("  init     %lf\n",duration(1,2));
+	printf("  forkexec %lf\n",duration(3,4));
+	printf("  all      %lf\n",duration(0,4));
+
 	return 0;
 }
 #endif
